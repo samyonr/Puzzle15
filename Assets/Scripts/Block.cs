f7846e25416531c89025d236df2739b64feb0975
SamyonR <samyon.ristov@ayyeka.com>
2020-04-25 23:22:15 +0300

    Adding sliding mechanics to blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-
+    public event System.Action<Block> OnBlockPressed;
+    public event System.Action OnFinishedMoving;
     public Vector3Int coord;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {  
+
     }
 
     public void Init(Vector3Int startingCoord, Texture2D image)
@@ -27,13 +28,34 @@ public class Block : MonoBehaviour
         GetComponent<MeshRenderer>().material.mainTexture = image;
     }
 
-    public event System.Action<Block> OnBlockPressed;
+    public void MoveToPisition(Vector3 target, float duration)
+    {
+        StartCoroutine(AnimateMove(target, duration));
+    }
 
     void OnMouseDown()
     {
         if (OnBlockPressed != null)
         {
             OnBlockPressed(this);
+        }
+    }
+
+    IEnumerator AnimateMove(Vector3 target, float duration)
+    {
+        Vector3 initalPos = transform.position;
+        float percent = 0;
+
+        while (percent < 1)
+        {
+            percent += Time.deltaTime / duration;
+            transform.position = Vector3.Lerp(initalPos, target, percent);
+            yield return null;
+        }
+
+        if (OnFinishedMoving != null)
+        {
+            OnFinishedMoving();
         }
     }
 }
--- Assets/Scripts/Puzzle.cs
+++ Assets/Scripts/Puzzle.cs
@@ -6,9 +6,10 @@ public class Puzzle : MonoBehaviour
 {
 
     public Texture2D image;
-
     public int blocksPerLine = 4;
     Block emptyBlock;
+    Queue<Block> inputs;
+    bool blockIsMoving;
 
     // Start is called before the first frame update
     void Start()
@@ -36,8 +37,8 @@ public class Puzzle : MonoBehaviour
 
                 Block block = blockObject.AddComponent<Block>();
                 block.OnBlockPressed += PlayerMoveBlockInput;
+                block.OnFinishedMoving += OnBlockFinishedMoving;
                 block.Init(new Vector3Int(x, y, 0), imageSlices[blocksPerLine - x - 1, blocksPerLine - y - 1]);
-                //block.coord = new Vector3Int(x, y, 0);
 
                 if (y == 0 && x == blocksPerLine - 1)
                 {
@@ -48,9 +49,24 @@ public class Puzzle : MonoBehaviour
         }
 
         Camera.main.transform.position = new Vector3(0, 1, -blocksPerLine * 2f);
+        inputs = new Queue<Block>();
     }
 
     void PlayerMoveBlockInput(Block blockToMove)
+    {
+        inputs.Enqueue(blockToMove);
+        MakeNextPlayerMove();
+    }
+
+    void MakeNextPlayerMove()
+    {
+        while (inputs.Count > 0 && !blockIsMoving)
+        {
+            MoveBlock(inputs.Dequeue());
+        }
+    }
+
+    void MoveBlock(Block blockToMove)
     {
         if ((blockToMove.coord - emptyBlock.coord).sqrMagnitude == 1)
         {
@@ -60,7 +76,15 @@ public class Puzzle : MonoBehaviour
 
             Vector3 targetPosition = emptyBlock.transform.position;
             emptyBlock.transform.position = blockToMove.transform.position;
-            blockToMove.transform.position = targetPosition;
+            blockToMove.MoveToPisition(targetPosition, .2f);
+            blockIsMoving = true;
         }
+    }
+
+    void OnBlockFinishedMoving()
+    {
+        blockIsMoving = false;
+
+        MakeNextPlayerMove();
     }
 }
