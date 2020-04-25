a6c76664ca70c471449a1a0ff4cc58b9f5f8d686
SamyonR <samyon.ristov@ayyeka.com>
2020-04-25 23:57:12 +0300

    Adding shuffling

--- Assets/Scripts/Puzzle.cs
+++ Assets/Scripts/Puzzle.cs
@@ -7,9 +7,19 @@ public class Puzzle : MonoBehaviour
 
     public Texture2D image;
     public int blocksPerLine = 4;
+    public int shuffleLength = 20;
+    public float defaultMoveDuration = .2f;
+    public float shuffleMoveDuration = .1f;
+
+    enum PuzzleState {Solved, Shuffling, InPlay};
+    PuzzleState state;
+
     Block emptyBlock;
+    Block[,] blocks;
     Queue<Block> inputs;
     bool blockIsMoving;
+    int shuffleMovesRemaining;
+    Vector3Int prevShuffleOffset;
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +30,15 @@ public class Puzzle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (state == PuzzleState.Solved && Input.GetKeyDown(KeyCode.Space))
+        {
+            StartShuffle();
+        }
     }
 
     void CreatePuzzle()
     {
+        blocks = new Block[blocksPerLine, blocksPerLine];
         Texture2D[,] imageSlices = ImageSlicer.GetSlices(image, blocksPerLine);
 
         for (int y = 0; y < blocksPerLine; y++)
@@ -39,6 +53,7 @@ public class Puzzle : MonoBehaviour
                 block.OnBlockPressed += PlayerMoveBlockInput;
                 block.OnFinishedMoving += OnBlockFinishedMoving;
                 block.Init(new Vector3Int(x, y, 0), imageSlices[blocksPerLine - x - 1, blocksPerLine - y - 1]);
+                blocks[x, y] = block;
 
                 if (y == 0 && x == blocksPerLine - 1)
                 {
@@ -54,29 +69,35 @@ public class Puzzle : MonoBehaviour
 
     void PlayerMoveBlockInput(Block blockToMove)
     {
-        inputs.Enqueue(blockToMove);
-        MakeNextPlayerMove();
+        if (state == PuzzleState.InPlay)
+        {
+            inputs.Enqueue(blockToMove);
+            MakeNextPlayerMove();
+        }
     }
 
     void MakeNextPlayerMove()
     {
         while (inputs.Count > 0 && !blockIsMoving)
         {
-            MoveBlock(inputs.Dequeue());
+            MoveBlock(inputs.Dequeue(), defaultMoveDuration);
         }
     }
 
-    void MoveBlock(Block blockToMove)
+    void MoveBlock(Block blockToMove, float duration)
     {
         if ((blockToMove.coord - emptyBlock.coord).sqrMagnitude == 1)
         {
+            blocks[blockToMove.coord.x, blockToMove.coord.y] = emptyBlock;
+            blocks[emptyBlock.coord.x, emptyBlock.coord.y] = blockToMove;
+
             Vector3Int targetCoord = emptyBlock.coord;
             emptyBlock.coord = blockToMove.coord;
             blockToMove.coord = targetCoord;
 
             Vector3 targetPosition = emptyBlock.transform.position;
             emptyBlock.transform.position = blockToMove.transform.position;
-            blockToMove.MoveToPisition(targetPosition, .2f);
+            blockToMove.MoveToPisition(targetPosition, duration);
             blockIsMoving = true;
         }
     }
@@ -85,6 +106,56 @@ public class Puzzle : MonoBehaviour
     {
         blockIsMoving = false;
 
-        MakeNextPlayerMove();
+        if (state == PuzzleState.InPlay)
+        {
+            MakeNextPlayerMove();
+        }
+        else if (state == PuzzleState.Shuffling)
+        {
+            if (shuffleMovesRemaining > 0)
+            {
+                MakeNextShuffleMove();
+            }
+            else
+            {
+                state = PuzzleState.InPlay;
+            }
+        }
+    }
+
+    void StartShuffle()
+    {
+        state = PuzzleState.Shuffling;
+
+        shuffleMovesRemaining = shuffleLength;
+
+        MakeNextShuffleMove();
+    }
+
+    void MakeNextShuffleMove()
+    {
+        Vector3Int[] offsets = {new Vector3Int(1, 0, 0),
+                                new Vector3Int(-1, 0, 0),
+                                new Vector3Int(0, 1, 0),
+                                new Vector3Int(0, -1, 0) };
+        int randomIndex = Random.Range(0, offsets.Length);
+
+        for (int i = 0; i < offsets.Length; i++)
+        {
+            Vector3Int offset = offsets[(randomIndex + i) % offsets.Length];
+            if (offset != prevShuffleOffset * -1)
+            {
+                Vector3Int moveBlockCoord = emptyBlock.coord + offset;
+
+                if (moveBlockCoord.x >= 0 && moveBlockCoord.x < blocksPerLine &&
+                    moveBlockCoord.y >= 0 && moveBlockCoord.y < blocksPerLine)
+                {
+                    MoveBlock(blocks[moveBlockCoord.x, moveBlockCoord.y], shuffleMoveDuration);
+                    shuffleMovesRemaining--;
+                    prevShuffleOffset = offset;
+                    break;
+                }
+            }
+        }
     }
 }
